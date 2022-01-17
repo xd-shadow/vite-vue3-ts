8575c924d78c177f7cddffb1a913523ce8550dfa
SHADOW <505029990@qq.com>
2022-01-17 10:24:54 +0800

    fix: fix indent

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,26 +1,26 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
   extends: [
     "eslint:recommended",
     "plugin:vue/essential",
     "plugin:@typescript-eslint/recommended",
     "prettier",
-    "plugin:prettier/recommended",
+    "plugin:prettier/recommended"
   ],
   parserOptions: {
     ecmaVersion: 13,
     parser: "@typescript-eslint/parser",
-    sourceType: "module",
+    sourceType: "module"
   },
   plugins: ["vue", "@typescript-eslint"],
   rules: {
-    indent: ["error", 2],
+    // indent: ["error", 2],
     "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
-    semi: ["error", "always"],
+    semi: ["error", "always"]
   },
-  parser: "vue-eslint-parser",
+  parser: "vue-eslint-parser"
 };
--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -3,7 +3,7 @@
 </template>
 
 <script lang="ts" setup>
-import {} from "vue";
+  import {} from "vue";
 </script>
 
 <style lang="scss" scoped></style>
